71c750f2d32263847651a44a2143961f33918258
jpcanto <jplopessk8@gmail.com>
2021-04-22 20:04:58 -0300

    fix: ajusta filtros

--- src/store/modules/patientsModule.js
+++ src/store/modules/patientsModule.js
@@ -13,9 +13,25 @@ export default {
     filteredPatients: [],
   },
   getters: {
-    GET_FILTERED_PATIENTS: (state) => (
-      filtering.filterByName(Array.from(state.patients), state.searchParam)
-    ),
+    GET_FILTERED_PATIENTS: (state) => {
+      let filteredPatients = state.patients;
+
+      switch (state.filterParam) {
+        case 0:
+          filteredPatients = state.patients;
+          break;
+        case 1:
+          filteredPatients = filtering.filterByUnpaids([...state.patients]);
+          break;
+        case 2:
+          filteredPatients = [...state.patients].sort(sorting.orderByName);
+          break;
+        default:
+          filteredPatients = state.patients;
+          break;
+      }
+      return filtering.filterByName(Array.from(filteredPatients), state.searchParam);
+    },
     GET_PATIENTS_NUMBER: (state) => state.patients.length || 0,
     GET_PATIENTS_FILTERED_NUMBER: (state) => {
       const filtered = filtering.filterByName([...state.patients], state.searchParam);
@@ -38,10 +54,9 @@ export default {
     DISPATCH_PATIENT_FILTER_PARAM: ({ commit }, payload) => {
       commit('SET_PATIENTS_LOADING', true);
       commit('SET_PATIENTS_FILTER_PARAM', payload);
-      commit('SET_PATIENTS_FILTERED', payload);
       commit('SET_PATIENTS_LOADING', false);
     },
-    DISPATCH_PATIENT_SEARCH_PARAM: ({ commit }, payload) => commit('SET_PATIENT_SEARCH_PARAM', payload),
+    DISPATCH_PATIENT_SEARCH_PARAM: ({ commit }, payload) => commit('SET_PATIENTS_SEARCH_PARAM', payload),
     DISPATCH_PATIENT_READY_TO_EDIT: ({ commit }, payload) => commit('SET_PATIENT_READY_TO_EDIT', payload),
   },
   mutations: {
@@ -56,21 +71,6 @@ export default {
     },
     SET_PATIENTS_FILTER_PARAM: (state, payload) => {
       state.filterParam = payload;
-    },
-    SET_PATIENTS_FILTERED: (state) => {
-      switch (state.filterParam) {
-        case 0:
-          return state.patients;
-        case 1:
-          state.filteredPatients = filtering.filterByUnpaids([...state.patients]);
-          break;
-        case 2:
-          state.filteredPatients = [...state.patients].sort(sorting.orderByName);
-          break;
-        default:
-          state.filteredPatients = state.patients;
-          break;
-      }
     },
     SET_PATIENTS_SEARCH_PARAM: (state, payload) => {
       state.searchParam = payload;
